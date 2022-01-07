6750abcdff5ac91fd752b45366eb36fe122f5812
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-01-07 13:35:17 +0200

    style faq section for mobile and add javascript interactivity for accordion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
+const faq = document.querySelectorAll(".faq__questions-question");
+const hover = document.querySelectorAll(".hover");
 
+faq.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+
+    if (faq.classList.contains("active")) {
+      faq.classList.remove("hover");
+    } else faq.classList.add("hover");
+    console.log(faq);
+  });
+});
