df09f8f52b47f091448d808bd86158c44cdd42e1
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-01-08 16:15:22 +0200

    create hamburger menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const faq = document.querySelectorAll(".faq__questions-question");
 const hover = document.querySelectorAll(".hover");
 
+const menuButton = document.querySelector(".menu");
+const nav = document.querySelector(".nav");
+const closeButton = document.querySelector(".close");
+
 faq.forEach((faq) => {
   faq.addEventListener("click", () => {
     faq.classList.toggle("active");
@@ -9,4 +13,12 @@ faq.forEach((faq) => {
       faq.classList.remove("hover");
     } else faq.classList.add("hover");
   });
+});
+
+menuButton.addEventListener("click", () => {
+  nav.classList.add("nav-active");
+});
+
+closeButton.addEventListener("click", () => {
+  nav.classList.remove("nav-active");
 });
