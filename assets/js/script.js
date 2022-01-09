9a13716c6c0fee446dd64938a0afd70310912bad
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-01-09 18:56:20 +0200

    add arrow to navigate to the top instantly

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,27 @@ const hover = document.querySelectorAll(".hover");
 const menuButton = document.querySelector(".menu");
 const nav = document.querySelector(".nav");
 const closeButton = document.querySelector(".close");
+const navLink = document.querySelectorAll(".nav-link");
 
 const formButton = document.querySelector(".form-send");
 const formMessage = document.querySelector(".form-notification");
 
 const body = document.querySelector("body");
 const lightModeToggleButton = document.querySelectorAll(".bx-moon");
+
+const upArrow = document.querySelector(".up-arrow");
+
+// arrow to go to the top instantly
+const scrollUp = () => {
+  const y = window.scrollY;
+  if (y >= 400) {
+    upArrow.classList.add("active");
+  } else {
+    upArrow.classList.remove("active");
+  }
+};
+
+window.addEventListener("scroll", scrollUp);
 
 // accordion
 faq.forEach((faq) => {
@@ -25,6 +40,14 @@ menuButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
   nav.classList.remove("nav-active");
+});
+
+navLink.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    if (nav.classList.contains("nav-active")) {
+      nav.classList.remove("nav-active");
+    }
+  });
 });
 
 // message on form submit
